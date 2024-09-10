065096a2adfdfa2ccf58a27d00bff3fdd66cc2d0
shin <ramuza0808@gmail.com>
2024-09-10 23:45:11 +0900

    repeat関数を入れて繰り返しを整理した。

--- 宝くじ運試し.py
+++ 宝くじ運試し.py
@@ -1,23 +1,28 @@
 import random
 #　それぞれの宝くじの数字を予測をします
+
+#repeat=◯,△,☓,をrandomで出力 
+def repeat():
+    print(random.sample(( '◯','△','☓', ) ,1))
+    
 takara = {'ナンバーズ3' :3 , 'ナンバーズ4' :4 ,'ビンゴ5' :5 , 'ロト6' :6 , 'ロト7' :7}
 a = list(takara.keys())
 print(a)
 print('ナンバーズ3')
 print(random.sample(range(0,10) ,3))
-print(random.sample(( '◯','△','☓', ) ,1))
+repeat()
 print('ナンバーズ4')
 print(random.sample(range(0,10) ,4))
-print(random.sample(( '◯','△','☓', ) ,1))
+repeat()
 print('ビンゴ5')
 print(random.sample(range(1,41) ,5))
-print(random.sample(( '◯','△','☓', ) ,1))
+repeat()
 print('ロト6')
 print(random.sample(range(1,44) ,6))
-print(random.sample(( '◯','△','☓', ) ,1))
+repeat()
 print('ロト7')
 print(random.sample(range(1,38) ,7))
-print(random.sample(( '◯','△','☓', ) ,1))
+repeat()
 
 x = 50
 y = 100
